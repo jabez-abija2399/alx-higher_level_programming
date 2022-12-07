98bec69d95d0861782e3ac421dcc084d000d7d7f
jabez-abija2399 <jabeznew2399@gmail.com>
2022-12-07 07:58:05 -0800

    djah

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 # 6-print_sorted_dictionary.py
 def print_sorted_dictionary(a_dictionary):
     """Print a dictionary by ordered keys."""
-    [print("{}: {}".format(k, a_dictionary[k])) for k in sorted(a_dictionary)]`:wq
+    [print("{}: {}".format(k, a_dictionary[k])) for k in sorted(a_dictionary)]
