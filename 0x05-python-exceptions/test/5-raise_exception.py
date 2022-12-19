ee628a9247b1715a925df4c17c97e0051c9b02d9
jabez-abija2399 <jabeznew2399@gmail.com>
2022-12-19 08:24:08 -0800

    all

--- 0x05-python-exceptions/test/5-raise_exception.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-# 5-raise_exception.py
-# yabets
-
-
-def raise_exception():
-    """Raise a TypeError exception."""
-    raise TypeError
--- 0x05-python-exceptions/test/6-raise_exception_msg.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-# 6-raise_exception_msg.py
-# yabets
-
-
-def raise_exception_msg(message=""):
-    """Raise a NameError exception with a message."""
-    raise NameError(message)
